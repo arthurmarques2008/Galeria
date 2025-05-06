54f8a20ae53860d28b3e801bea7230bd6df1872f
arthurmarques2008 <arthur.marques@aluno.ifce.edu.br>
2025-05-06 11:12:10 -0300

    Inserimos duas páginas novas ao projeto

--- Galeria/galeria/urls.py
+++ Galeria/galeria/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('sobre/', views.sobre, name='sobre'),
-
-
-
+    path('descricao/', views.descricao, name='descricao'),
+    path('aula/', views.aula, name='aula')
+    
 ]
--- Galeria/galeria/views.py
+++ Galeria/galeria/views.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.shortcuts import render
 
 def index(request):
@@ -5,3 +6,15 @@ def index(request):
 
 def sobre(request):
     return render(request, 'sobre.html')
+
+def descricao(request):
+    return render(request, 'descricao.html')
+
+def aula(request):
+    
+    hora_atual = timezone.now().hour
+    if hora_atual <= 9 or hora_atual >= 11:
+        status_aula = 'em_aula'
+    else:
+        status_aula = 'fora_da_aula'
+    return render(request, 'aula.html', {'status_aula': status_aula})
